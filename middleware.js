74c38756b89ad12b8a541f75b5c6f5e92ee86412
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-01-27 08:46:51 +0530

    Final Commit

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware";
 export const config = {
     matcher: [
-        "/profile", "/activity-points", "/course-registration", "/faculty/profile", "/enrolled", "/exams", "/health", "/internship",
-        "/proctormeet", "/scholarship"
+        "/profile", "/activity-points", "/course-registration", "/faculty/profile", "/handling", "/exams", "/extra-curricular", "/internship",
+        "/proctor-meet", "/scholarship"
     ]
 }
